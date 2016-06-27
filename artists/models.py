a86f4efc163ca56a8d8c5ac73ad2eff40c71a13d
Vadim Vakhovsky <vadim.vakhovsky@gmail.com>
2016-06-27 21:07:08 +0300

    Defined models and added migrations

--- artists/models.py
+++ artists/models.py
@@ -2,4 +2,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Artist(models.Model):
+  name = models.CharField(max_length=64)
+
+
+class Album(models.Model):
+  artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+  name = models.CharField(max_length=64)
+  image_url = models.CharField(max_length=255)
+
