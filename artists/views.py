0acff25b66f5e93867ba7fb119e2e77720f40ab7
Vadim Vakhovsky <vadim.vakhovsky@gmail.com>
2016-06-27 23:40:54 +0300

    Add artist/albums

--- artists/views.py
+++ artists/views.py
@@ -1,6 +1,9 @@
+import urllib, json
 from django.shortcuts import render
 
 from django.http import HttpResponse
+
+from .models import Artist, Album
 
 
 def index(request):
@@ -10,5 +13,15 @@ def add_artist(request):
 	return render(request, 'artists/add.html', {})
 
 def process_add_artist(request):
-	print request.POST
+	artist_name = request.POST.get('name')
+	url         = "https://itunes.apple.com/search?term=" + urllib.quote_plus(artist_name) +"&media=music&entity=album"
+	response    = urllib.urlopen(url)
+	albums_data = json.loads(response.read())
+
+	artist = Artist(name=artist_name)
+	artist.save()
+
+	for result in albums_data.get('results'):
+		artist.album_set.create(name=result['collectionName'], image_url=result['artworkUrl100'])
+
 	return HttpResponse('ok');
