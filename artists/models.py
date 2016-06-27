9652aeed620018c3c14d2dd292e1087424fc7d3a
Vadim Vakhovsky <vadim.vakhovsky@gmail.com>
2016-06-27 21:19:49 +0300

    Return more useful representation of Artist and Album models

--- artists/models.py
+++ artists/models.py
@@ -5,9 +5,15 @@ from django.db import models
 class Artist(models.Model):
   name = models.CharField(max_length=64)
 
+  def __str__(self):
+  	return self.name
+
 
 class Album(models.Model):
   artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
   name = models.CharField(max_length=64)
   image_url = models.CharField(max_length=255)
+
+  def __str__(self):
+  	return self.name
 
