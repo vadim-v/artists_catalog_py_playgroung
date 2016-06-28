d90aacdca4716dc7614ed30a3e5bc3baa72d08aa
Vadim Vakhovsky <vadim.vakhovsky@gmail.com>
2016-06-28 15:45:46 +0300

    Artists: browse & show albums * whitespace changes

--- artists/urls.py
+++ artists/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
   url(r'^$', views.index, name='index'),
   url(r'^add/$', views.add_artist, name='add_artist'),
+  url(r'^browse/$', views.browse_artist, name='browse_artist'),
+  url(r'^fetch_albums/(?P<artist_id>[0-9]+)/$', views.fetch_albums, name='fetch_albums'),
   url(r'^process_add_artist/$', views.process_add_artist, name='process_add_artist'),
 ]
--- artists/views.py
+++ artists/views.py
@@ -7,31 +7,41 @@ from .models import Artist, Album
 
 
 def index(request):
-	return render(request, 'artists/index.html', {})
+  return render(request, 'artists/index.html', {})
+
+def browse_artist(request):
+  artists = Artist.objects.all()
+  return render(request, 'artists/browse.html', { 'artists': artists })
+
+def fetch_albums(request, artist_id):
+  artist = Artist.objects.get(id=artist_id)
+  albums = artist.album_set.all()
+  return render(request, 'artists/albums.html', { 'albums': albums })
 
 def add_artist(request):
-	return render(request, 'artists/add.html', {})
+  return render(request, 'artists/add.html', {})
+
 
 def process_add_artist(request):
-	artist_name = request.POST.get('name')
+  artist_name = request.POST.get('name')
 
-	try:
-		url         = "https://itunes.apple.com/search?term=" + urllib.quote_plus(artist_name) +"&media=music&entity=album"
-		response    = urllib.urlopen(url)
-		albums_data = json.loads(response.read())
+  try:
+    url = "https://itunes.apple.com/search?term=" + urllib.quote_plus(artist_name) + "&media=music&entity=album"
+    response = urllib.urlopen(url)
+    albums_data = json.loads(response.read())
 
-		if albums_data.get('resultCount') > 0:
-			artist = Artist(name=artist_name)
-			artist.save()
+    if albums_data.get('resultCount') > 0:
+      artist = Artist(name=artist_name)
+      artist.save()
 
-			for result in albums_data.get('results'):
-				artist.album_set.create(name=result['collectionName'], image_url=result['artworkUrl100'])
+      for result in albums_data.get('results'):
+        artist.album_set.create(name=result['collectionName'], image_url=result['artworkUrl100'])
 
-		else:
-			return HttpResponse('err')
+    else:
+      return HttpResponse('err')
 
-	except:
-		return HttpResponse('err')
+  except:
+    return HttpResponse('err')
 
-	else:
-		return HttpResponse('ok')
+  else:
+    return HttpResponse('ok')
